54ee0bd925762914257914e677c616383e16f3dc
fcupen <f-918@hotmail.com>
2021-08-08 23:12:43 +0200

    dist folder

---
+++ dist/precache-manifest.db173d22b40e144f3cefa7b1a6e67dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388c507be84d549aec4d",
+    "url": "/css/app.db5ffa00.css"
+  },
+  {
+    "revision": "fa18c2d0e322f55a81e3",
+    "url": "/css/chunk-vendors.99658ae2.css"
+  },
+  {
+    "revision": "487a15caf570ab25ec9c21a472f31a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388c507be84d549aec4d",
+    "url": "/js/app.6ea0d2ac.js"
+  },
+  {
+    "revision": "fa18c2d0e322f55a81e3",
+    "url": "/js/chunk-vendors.28028ff9.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
